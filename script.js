021f0039ffb79c375f0838f16da8cc7ec85920ec
evan-azemard <evan-azemard@laplateforme.io>
2022-03-29 17:24:51 +0200

    fusions des deux projet

---
+++ script.js
@@ -0,0 +1,26 @@
+// const section1 = document.querySelector("#vagues")
+//
+//
+// console.log(section1)
+//
+//
+//
+//
+// const createVague = () => {
+//     const cube = document.createElement("img")
+//     cube.src = "vaguevague-removebg-preview.png"
+//     cube.classList.add("contentSpan")
+//
+//     section1.appendChild(cube)
+//
+//     setTimeout(() => {
+//         cube.remove()
+//     }, 3000)
+// }
+//
+//
+//
+// setInterval(() => {
+//     createVague()
+//
+// }, 1000)
